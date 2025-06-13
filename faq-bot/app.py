48380225e05cbbf2b35368d13307227a5b2daeba
Nicolaj Dedenroth Aalykke <nicolaj.it@icloud.com>
2025-06-13 10:37:02 +0200

    fix

--- faq-bot/app.py
+++ faq-bot/app.py
@@ -1,8 +1,12 @@
 import gradio as gr
+import os
 from faq_bot import chat_with_faq  # reuse the function from your backend
 
 def chat_interface(message, history):
     response = chat_with_faq(message)
     return response
 
-gr.ChatInterface(fn=chat_interface, type="messages").launch()
+port = int(os.environ.get("PORT", 80))
+
+
+gr.ChatInterface(fn=chat_interface, type="messages").launch(server_name="0.0.0.0", server_port=port)
